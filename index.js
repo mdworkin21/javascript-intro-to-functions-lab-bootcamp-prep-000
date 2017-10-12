fa7c7147ee43f5a0f39ca013f5382ab444c13bc4
Matthew Dworkin <mdworkin21@gmail.com>
2017-10-12 19:58:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ function logWhisper(string){
   console.log(string.toLowerCase())
 }
 
-var lowercase = "hello!"
-var uppercase = "HELLO!"
+/*var lowercase = "hello!"
+var uppercase = "HELLO!" */
 
 function sayHiToGrandma(string){
 
