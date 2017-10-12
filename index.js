28c920746331c6a466ef6bd1ae8db62030894f36
Matthew Dworkin <mdworkin21@gmail.com>
2017-10-12 18:22:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function sayHiToGrandma(string){
   var lowercase = "hello!"
   var uppercase = "HELLO!"
 
-  if (lowercase.toLowerCase() === false){
+  if (lowercase.toLowerCase() === lowercase){
     return "I can't hear you!"
   } else {
     return "YES INDEED"
